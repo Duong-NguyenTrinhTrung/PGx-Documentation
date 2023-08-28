7ec9867a614af6246b52bebfe9b61f5ee2fda40d
nguyentrinhtrungduong <nguyentrinhtrungduong@gmail.com>
2023-08-28 10:55:34 +0200

    Editing build os info4

--- conf.py
+++ conf.py
@@ -22,7 +22,6 @@ templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
-build_os = "macos"
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
